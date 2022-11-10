4fbba3f18fdf1e9192a957aea3b2e30dd8728112
Ashish98th <ashishnathofficia98@gmail.com>
2022-11-10 14:23:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Cars from './cars.json';
+
+import './Cars.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   
+   <div className='container'>
+
+   {
+      Cars.map(car=>{
+        return(
+         <div key={car.id} >
+          
+          <p>{car.bodyType}</p> <br/>
+          <h2>{car.modelName}</h2> <p>{car.modelType}</p>
+        
+        <img src={car.imageUrl} alt="loading..."/>
+
+        <br/>
+
+
+         </div>
+        )
+      })
+    }
+
+
+   </div>
+  
     </div>
   );
 }
